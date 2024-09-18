011ae91740d1dd535374961332b58624f672b573
Poornima Konda Ramkumar <poornima.ramkumar@accolade.com>
2024-09-18 11:33:23 -0400

    update here too

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:i_am_rich/main.dart';
+import 'package:flutter_contact_card/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
